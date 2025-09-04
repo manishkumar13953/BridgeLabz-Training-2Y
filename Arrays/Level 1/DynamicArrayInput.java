64ec06813dd6b227b8b0faf92b9a345585935ddc
manish__24100 <166864864+manishkumar13953@users.noreply.github.com>
2025-09-04 16:14:46 +0530

    Add files via upload

---
+++ Arrays/Level 1/DynamicArrayInput.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DynamicArrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] numbers = new double[10];
+        double total = 0.0;
+        int index = 0;
+        
+        while (true) {
+            System.out.print("Enter a number: ");
+            double input = sc.nextDouble();
+            
+            if (input <= 0) {
+                break;
+            }
+            
+            if (index == 10) {
+                break;
+            }
+            
+            numbers[index] = input;
+            index++;
+        }
+        
+        for (int i = 0; i < index; i++) {
+            total += numbers[i];
+            System.out.println("Number " + (i + 1) + ": " + numbers[i]);
+        }
+        
+        System.out.println("Total: " + total);
+    }
+}
